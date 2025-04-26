569b09c4e3d6a3b33881d692a406670465e3e793
Ekzitus <nekrasov99@gmail.com>
2025-04-26 14:48:11 +0500

    Добавил контроллер Home и представление Index для настройки маршрутизации в ASP.NET Core MVC

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Настраиваем маршрутизацию для MVC
+app.UseStaticFiles(); // Поддержка статических файлов из wwwroot
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
